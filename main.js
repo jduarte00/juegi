dedc8086296509c1935a9060617bab342076e067
duarte <dumejo92@gmail.com>
2018-09-05 16:03:05 -0500

    demo

---
+++ main.js
@@ -0,0 +1,388 @@
+//set game score
+let score = 0;
+let vidas = 3;
+let frames = 0;
+let seconds = 0;
+//set canvas
+let canvas = document.getElementById('my-canvas');
+let ctx = canvas.getContext('2d');
+canvas.width = '800';
+canvas.height ='600';
+
+//imagenes monito
+let image1 = new Image();
+image1.src = "img/orc0.png";
+let image2 = new Image();
+image2.src = "img/orc1.png";
+
+
+//set images
+let images = {
+    bg: 'img/topwall.png'
+}
+
+// 
+let playerBullets = [];
+let enemies = [];
+
+
+// set the board(i.e. background)
+class Board{
+    constructor(){
+        this.x = 0;
+        this.y = 0;
+        this.width = canvas.width;
+        this.height = canvas.height;
+        this.image = document.createElement('img');
+        this.image.src = images.bg;
+        this.image.height = 500;
+        this.image.onload = function(){
+            this.draw();
+        }
+        this.frames = 0;
+        this.image.onload = () => {
+            this.draw()
+        }
+    }
+
+    draw(){
+        //draw the background
+        /* this.x -= .5;
+        if (this.x < - this.width) return this.x = 0; */
+        ctx.clearRect(0,0, this.width, this.height);
+        ctx.drawImage(this.image, this.x, this.y);
+        /* ctx.drawImage(this.image, this.width + this.x, this.y); */
+        // display the number of points or fps
+        ctx.fillStyle = "lightgreen";
+        ctx.fillText('Score: ' +score, 10, 20);
+        ctx.fillStyle = "white";
+        ctx.fillText('Seconds: ' +seconds, 10, 40);
+        ctx.fillStyle = "red";
+        ctx.fillText('Lives: ' + vidas, 10, 60);
+        frames++;
+        seconds = parseInt(frames/60);
+    }
+}
+
+let myBoard = new Board();
+
+// Draw monito
+let monito = {
+    width: 43,
+    height: 50,
+    x: 30,
+    y: 50,
+    selectedImage: image1,
+    haciaAbajo: false,
+    jump_y: this.y,
+    updater: 0,
+    limitupdater: 10,
+   
+    draw(){
+        this.updater ++;
+        if (this.updater >= this.limitupdater){
+            if (this.selectedImage === image1) {
+                this.selectedImage = image2
+            } else {this.selectedImage = image1}
+            this.updater = 0;
+        }
+        //this.y =  myBoard.height - monito.height -10
+        ctx.drawImage(this.selectedImage, this.x, this.y);
+    },
+
+    moveForward(){
+        if (this.x >= myBoard.width - this.width) {
+            this.x = myBoard.width -this.width
+        } 
+        this.x += 5;
+    },
+
+    moveBackward(){
+        if (this.x <= 0){
+            this.x = 0;
+        }
+        this.x -= 5;
+    },
+    
+    idle(){
+        
+    },
+
+    moveUp(){
+        if (this.y <= 0){
+            this.y = 0;
+        }
+        this.y -= 5;
+    },
+
+    moveDown(){
+        if (this.y >= myBoard.height - this.height -5){
+            this.y = myBoard.height-this.height -5;
+        }
+        this.y += 5;
+    },
+
+    shoot(){
+        var bulletPosition = this.midpoint();
+        
+        playerBullets.push( new Bullet({
+            speed: 5,
+            x: bulletPosition.x,
+            y: bulletPosition.y
+        }));
+    },
+
+    midpoint(){
+        return {
+            x: this.x + this.width/2,
+            y: this.y +this.height/2
+        };
+    }
+};
+// proyectiles
+
+function Bullet(I){
+    I.active = true;
+    I.xVelocity = I.speed;
+    I.yVelocity = 0;
+    I.width = 16;
+    I.height = 16;
+    I.image = new Image();
+    I.image.src = "img/apple.png"
+
+
+    I.inBounds = function() {
+        return I.x >= 0 && I.x <= canvas.width && I.y >= 0 && I.y <= canvas.height;
+    };
+
+    I.draw = function() {
+        
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+    };
+
+    I.update = function(){
+        I.x += I.xVelocity;
+        I.y += I.yVelocity;
+
+        I.active = I.active && I.inBounds();
+    };
+
+    return I;
+} 
+
+//enemigos
+class Enemigos{
+    constructor(y){
+        this.x = canvas.width;
+        this.y = y;
+        this.image = new Image();
+        this.id;
+        this.active = true;
+        this.explosion = new Image();
+        this.explosion.src = 'img/explosion.png';
+        this.wave = true;
+        this.counter = 0;
+    }
+
+    draw(){
+        this.x -=2;
+        if (this.counter % 100 === 0 && this.wave === true){
+            this.y += this.width;
+            this.wave = false;
+        } else if (this.counter % 100 === 0 && this.wave === false){
+            this.y -= this.width;
+            this.wave = true;
+        }
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+        this.counter++;
+    }
+
+    inBound(){
+        if (this.x >= 0 && this.x <= canvas.width && this.active === true){
+            return this.active = true;
+        }
+        this.active = false;
+    }
+
+    exp(){
+        this.active = false;
+        ctx.drawImage(this.explosion, this.x, this.y, this.width, this.height);
+    }
+}
+
+function generateEnemigos(){
+    if (frames % 200 === 0) {
+        let y = Math.floor(Math.random()*(canvas.height - 100));
+        let enemigo = new Enemigos(y);
+        enemigo.image.src = 'img/zombie.png';
+        enemigo.width = 68;
+        enemigo.height = 34;
+        enemigo.id = "zombie";
+        enemies.push(enemigo);
+    } else if (frames % 500 === 0) {
+        let y = Math.floor(Math.random()*(canvas.height - 100));
+        let enemigo = new Enemigos(y);
+        enemigo.image.src = 'img/satanas.png';
+        enemigo.width = 64;
+        enemigo.height = 64;
+        enemigo.id = "satan";
+        enemies.push(enemigo);
+    } else if (frames % 650 === 0){
+        let y = Math.floor(Math.random()*(canvas.height - 100));
+        let enemigo = new Enemigos(y);
+        enemigo.image.src = 'img/cow.png';
+        enemigo.width = 66;
+        enemigo.height = 38;
+        enemigo.id = "cow";
+        enemies.push(enemigo);   
+    }
+}
+
+//ckeck collitions
+function collides(a, b) {
+    return a.x < b.x + b.width &&
+         a.x + a.width > b.x &&
+         a.y < b.y + b.height &&
+         a.y + a.height > b.y;
+}
+
+function handleCollisions() {
+    playerBullets.forEach(function(bullet){
+        enemies.forEach(function(enemigo){
+            if (collides(bullet, enemigo)){
+                enemigo.exp();
+                bullet.active = false;
+                if(enemigo.id === "zombie"){
+                    score += 1;
+                } else if (enemigo.id === "satan"){
+                    score += 50;
+                } else if (enemigo.id === "cow") {
+                    score -= 100;
+                }
+            }
+        });
+    });
+    enemies.forEach(function(enemies){
+        if (collides(monito, enemies)){
+            enemies.active = false;
+            vidas -= 1;
+        }
+    })
+}
+
+//check if win or lose
+function checkIfWin(){
+    if (vidas <= 0){
+        console.log('perdiste');
+        //falta poner aqui una función para que detenga el juego
+    } if (seconds >= 90) {
+        console.log('tu score fue de ' + score);
+        //poner aqui función para detener el juego
+    }
+}
+// set interval
+setInterval(function(){
+    //board
+    myBoard.draw();
+    //monito
+    monito.draw();
+    //balas
+    playerBullets.forEach(function(bullet){
+        bullet.update();
+    });
+    playerBullets = playerBullets.filter(function(bullet){
+        return bullet.active;
+    });
+    playerBullets.forEach(function(bullet){
+        bullet.draw();
+    });
+    //enemigos
+    generateEnemigos();
+    enemies = enemies.filter(function(enemigo){
+        return enemigo.inBound()
+    });
+    enemies.forEach(function(enemigo){
+        enemigo.draw();
+    });
+    handleCollisions();
+    //checar si perdiste o ganaste
+    checkIfWin();
+    
+
+}, 1000/60)
+
+// determine two or more keys
+let multipleKeys = {}; //tampoco funciona
+onkeydown = onkeyup = function(e){
+    multipleKeys[e.keyCode] = e.type == 'keydown';
+}
+
+
+//event listeners
+addEventListener('keydown', function(e){
+    if(e.keyCode === 39){
+        if (multipleKeys[38]){
+            if(multipleKeys[32]){
+                monito.moveUp();
+                monito.moveForward();
+                montio.shoot();
+            }
+            //monito.moveUpForward();
+            monito.moveUp();
+            monito.moveForward()
+        } else if (multipleKeys[40]){
+            monito.moveDown();
+            monito.moveForward();
+        } 
+        else {
+            monito.moveForward();
+        }
+        return;
+    } else if (e.keyCode === 37){
+        if (multipleKeys[38]) {
+            monito.moveBackward();
+            monito.moveUp();
+        } else if (multipleKeys[40]){
+            monito.moveBackward();
+            monito.moveDown();
+        } 
+        else {
+            monito.moveBackward();
+        }
+        return;
+    } else if (e.keyCode === 38){
+        if (multipleKeys[39]){
+            //monito.moveUpForward();
+            monito.moveUp();
+            monito.moveForward();
+        } else if (multipleKeys[37]){
+            monito.moveUp();
+            monito.moveBackward();
+        }
+        else {
+            if (multipleKeys[32]){
+                monito.moveUp();
+                montio.shoot();
+
+            }
+            monito.moveUp();
+        }
+        return;
+    } else if (e.keyCode === 40) {
+        if (multipleKeys[37]) {
+            monito.moveDown();
+            monito.moveBackward();
+        } else if (multipleKeys[39]){
+            monito.moveDown();
+            monito.moveForward();
+        } 
+        else {
+            monito.moveDown();
+        }
+        
+        return;
+    } else if (e.keyCode === 32) {
+        monito.shoot();
+    }
+})
+
